19ba56746b24cbbd23b4c76c91a59f30fe00e2f4
Aaron Croissette <aaron@kinetech.llc>
2024-01-26 09:37:52 -0500

    feat: init stack

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,15 +1,18 @@
 import pulumi
 import pulumi_aws as aws
 
-# Load environment variables
-config = pulumi.Config()
-aws_region = config.require('aws_region')
-aws_endpoint = config.require('aws_endpoint')
+# # Load environment variables
+# config = pulumi.Config()
+# aws_region = config.require('aws_region')
+# aws_endpoint = config.require('aws_endpoint')
+
+# # Configure AWS provider to use LocalStack
+# aws_provider = aws.Provider('localstack', region=aws_region, 
+#                             endpoint=aws_endpoint, access_key='test', 
+#                             secret_key='test')
 
 # Configure AWS provider to use LocalStack
-aws_provider = aws.Provider('localstack', region=aws_region, 
-                            endpoint=aws_endpoint, access_key='test', 
-                            secret_key='test')
+aws_provider = aws.Provider('localstack')
 
 # Create an S3 bucket
 bucket = aws.s3.Bucket('my-pulumi-bucket', opts=pulumi.ResourceOptions(provider=aws_provider))
--- tests/pulumi_bucket.js
+++ tests/pulumi_bucket.js
@@ -3,11 +3,6 @@
 import { $ } from "bun"
 console.log('Running Pulumi resource check...')
 
-const pulumiVersion = (await $`pulumilocal version`.text()).trim()
-console.log(`Pulumi: ${pulumiVersion}`)
-
-if (!pulumiVersion) throw 'Invalid Pulumi version.'
-
 const awsCliVersion = (await $`aws --version`.text()).trim()
 console.log(`AWS CLI: ${awsCliVersion}`)
 
@@ -18,7 +13,14 @@ $.cwd('./pulumi')
 const pipResults = await $`pip install -r requirements.txt`
 if (pipResults.exitCode !== 0) throw pipResults.stderr.toString()
 
-const pulumiResults = await $`pulumilocal up --yes --non-interactive `
+const pulumiVersion = (await $`pulumilocal version`.text()).trim()
+console.log(`Pulumi: ${pulumiVersion}`)
+
+if (!pulumiVersion) throw 'Invalid Pulumi version.'
+
+await $`pulumi login --local`
+
+const pulumiResults = await $`pulumilocal up --yes --non-interactive`
 if (pulumiResults.exitCode !== 0) throw pulumiResults.stderr.toString()
 
 const bucketName = (await $`pulumilocal stack output bucket_name`.text()).trim()
