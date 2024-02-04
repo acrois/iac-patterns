510c3ae601c5961dab871d4c53734f34dfa3f4f5
Aaron Croissette <aaron@kinetech.llc>
2024-02-04 16:49:20 -0500

    feat: working pulumi test case (#1)
    * feat: working pulumi test case
    
    * test: print directory
    
    * test: print directory
    
    * test: print directory
    
    * feat: add logging to pulumi and output directory artifact for all test steps
    
    * fix: unique output per test step script name
    
    * fix: clean branch name for output artifact
    
    * fix: script name passing in workflow
    
    * fix: more output changes
    
    * fix: output path
    
    * feat: create stack if not exists, let's try it out
    
    * fix: reduce verbosity

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,21 +1,42 @@
 import pulumi
 import pulumi_aws as aws
+import os
+# import debugpy
 
-# # Load environment variables
-# config = pulumi.Config()
+# for a in os.environ:
+#     print('Var: ', a, 'Value: ', os.getenv(a))
+
+# debugpy.listen(("0.0.0.0", 5678))
+# print("debugpy is listening, attach by pressing F5 or ►")
+
+# debugpy.wait_for_client()
+# print("Attached to debugpy!")
+
+# Load environment variables
+config = pulumi.Config()
 # aws_region = config.require('aws_region')
 # aws_endpoint = config.require('aws_endpoint')
 
-# # Configure AWS provider to use LocalStack
-# aws_provider = aws.Provider('localstack', region=aws_region, 
-#                             endpoint=aws_endpoint, access_key='test', 
-#                             secret_key='test')
-
 # Configure AWS provider to use LocalStack
-aws_provider = aws.Provider('localstack')
+# aws_provider = aws.Provider(
+#     'localstack',
+#     region=os.getenv('AWS_REGION'), 
+#     # endpoint=os.getenv('AWS_ENDPOINT'),
+#     access_key=os.getenv('AWS_ACCESS_KEY_ID'), 
+#     secret_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+# )
+
+# aws_provider = aws.Provider('localstack')
+
+# opts = pulumi.ResourceOptions(provider=aws_provider)
+
+# def dump(obj):
+#   for attr in dir(obj):
+#     print("obj.%s = %r" % (attr, getattr(obj, attr)))
+# dump(opts)
 
 # Create an S3 bucket
-bucket = aws.s3.Bucket('my-pulumi-bucket', opts=pulumi.ResourceOptions(provider=aws_provider))
+bucket = aws.s3.Bucket('my-pulumi-bucket')
 
 # Export the bucket name
 pulumi.export('bucket_name', bucket.id)
--- tests/pulumi_bucket.js
+++ tests/pulumi_bucket.js
@@ -8,6 +8,8 @@ console.log(`AWS CLI: ${awsCliVersion}`)
 
 if (!awsCliVersion) throw 'Invalid AWS CLI version.';
 
+// await $`env`
+
 $.cwd('./pulumi')
 
 const pipResults = await $`pip install -r requirements.txt`
@@ -18,13 +20,17 @@ console.log(`Pulumi: ${pulumiVersion}`)
 
 if (!pulumiVersion) throw 'Invalid Pulumi version.'
 
+await $`ls`
 await $`pulumi login --local`
+const pulumiStackResult = await $`pulumi stack select -c dev`
+if (pulumiStackResult.exitCode !== 0) throw pulumiStackResult.stderr.toString()
 
-const pulumiResults = await $`pulumilocal up --yes --non-interactive`
+const pulumiResults = await $`pulumi up -s dev -v 3 --yes --non-interactive --logtostderr --logflow --tracing=file:../output/pulumi-up.trace`
+
 if (pulumiResults.exitCode !== 0) throw pulumiResults.stderr.toString()
 
-const bucketName = (await $`pulumilocal stack output bucket_name`.text()).trim()
+const bucketName = (await $`pulumi stack output bucket_name`.text()).trim()
 console.log(`Checking if S3 bucket ${bucketName} exists...`)
 
-const bucketResults = await $`aws s3 ls s3://${bucketName}`
+const bucketResults = await $`awslocal s3 ls s3://${bucketName}`
 if (bucketResults.exitCode !== 0) throw bucketResults.stderr.toString()
